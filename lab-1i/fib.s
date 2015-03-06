893e3a5c4932c38f3d9f4e347d6d081a7fe52f74
Edward Hsu <hsuedw@gmail.com>
2015-03-06 15:33:50 +0800

    non-recursive fibonacci: get rid of r6 and have the same answer

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -11,27 +11,23 @@
 fibonacci:
 	@ ADD/MODIFY CODE BELOW
 	@ PROLOG
-	push {r3, r4, r5, r6, lr}
+	push {r3, r4, r5, lr}
 
 	mov r3, #-1	@ r3 is previous
 	mov r4, #1	@ r4 is result
-	mov r5, #0	@ r5 is idx
-	mov r6, #0	@ r6 is sum
-	add r0, #1
 .LOOP:
-	cmp r5, r0
-	beq .FINISH
-
-	add r6, r4, r3	@ sum = result + previous
+	add r5, r4, r3	@ sum = result + previous
 	mov r3, r4	@ previous = result
-	mov r4, r6	@ result = sum
-	add r5, #1	@ idx++
+	mov r4, r5	@ result = sua
+
+	subs r0, #1
+	blt  .FINISH
 	b   .LOOP
 
 .FINISH:
 	mov r0, r4
 
-	pop {r3, r4, r5, r6, pc}		@EPILOG
+	pop {r3, r4, r5, pc}		@EPILOG
 
 	@ END CODE MODIFICATION
 
