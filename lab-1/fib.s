59f35694d6a34c4080ed3ca57c02288b0dc914df
Edward Hsu <hsuedw@gmail.com>
2015-03-06 19:30:38 +0800

    use Thumb2 conditional code to reduce the number of instructions.

--- lab-1/fib.s
+++ lab-1/fib.s
@@ -13,15 +13,13 @@ fibonacci:
 	@ PROLOG
 	push {r3, r4, r5, lr}
 
-	@ R4 = R0 - 0 (update flags)
-	@ if(R0 <= 0) goto .L3 (which returns 0)
-	subs r4, r0, #0
-	ble  .L3
+	subs  r4, r0, #0
+	it    lt
+	movlt r0, #0
+	beq   .FINISH
 
-	@ Compare R4 wtih 1
-	@ If R4 == 1 goto .L4 (which returns 1)
-	cmp r4, #1
-	beq .L4
+	cmp   r4, #1
+	beq   .FINISH 
 
 	@ R0 = R4 - 1
 	@ Recursive call to fibonacci with R4 - 1 as parameter
@@ -38,16 +36,8 @@ fibonacci:
 	@ R0 = R5 + R0 (update flags)
 	adds r0, r5, r0
 
+.FINISH:
 	pop {r3, r4, r5, pc}		@EPILOG
-
-	@ END CODE MODIFICATION
-.L3:
-	mov r0, #0			@ R0 = 0
-	pop {r3, r4, r5, pc}		@ EPILOG
-
-.L4:
-	mov r0, #1			@ R0 = 1
-	pop {r3, r4, r5, pc}		@ EPILOG
-
+	
 	.size fibonacci, .-fibonacci
 	.end
